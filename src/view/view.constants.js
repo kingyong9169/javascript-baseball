6fa74937475d7862c09a291f595f1c2330ac36fc
동동이 <drdragon777@naver.com>
2022-12-14 22:26:19 +0900

    chore - remove new line string

--- src/view/view.constants.js
+++ src/view/view.constants.js
@@ -1,5 +1,5 @@
 const MESSAGE = Object.freeze({
-  GAME_START: "숫자 야구 게임을 시작합니다.\n",
+  GAME_START: "숫자 야구 게임을 시작합니다.",
   NUM_INPUT: "숫자를 입력해주세요 : ",
   GAME_WIN:
     "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
