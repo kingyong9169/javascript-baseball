695f043da8e2a7e73d2de8ec620d52b648ee80c8
동동이 <drdragon777@naver.com>
2022-12-14 21:51:36 +0900

    feat - add initial App method(play, inputBaseBallNum)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const Computer = require("./model/Computer");
+const BaseBallNum = require("./model/BaseBallNum");
+const InputView = require("./view/InputView");
+const OutputView = require("./view/OutputView");
+
 class App {
-  play() {}
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  play() {
+    OutputView.printStart();
+    this.inputBaseBallNum();
+  }
+
+  #inputBaseBallNumCallback = (num) => {
+    this.input = new BaseBallNum(num);
+  };
+
+  inputBaseBallNum() {
+    InputView.readBaseBallNum(this.#inputBaseBallNumCallback);
+  }
 }
 
 module.exports = App;
