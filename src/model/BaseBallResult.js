dd3c01556dd587526ec0b9ad5889c628b4a1328c
동동이 <drdragon777@naver.com>
2022-12-14 22:11:22 +0900

    feat - add initial BaseBallResult module

---
+++ src/model/BaseBallResult.js
@@ -0,0 +1,16 @@
+class BaseBallResult {
+  constructor(computer, num) {
+    this.result = this.#makeResult(computer, num);
+  }
+
+  #makeResult(computer, num) {
+    const result = { strike: 0, ball: 0 };
+    return [...computer].reduce((acc, cur, idx) => {
+      if (num[idx] === cur) return { ...acc, strike: acc.strike + 1 };
+      if (num.includes(cur)) return { ...acc, ball: acc.ball + 1 };
+      return acc;
+    }, result);
+  }
+}
+
+module.exports = BaseBallResult;
